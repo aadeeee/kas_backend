00394cc54596c829bc8eef534b4b43868740549e
None <dhea.r.marpaung@gmail.com>
2024-06-23 11:41:57 +0700

    updateTransaction

--- model/transaction.js
+++ model/transaction.js
@@ -1,14 +1,20 @@
 import mongoose from "mongoose";
 
-const TransactionSchema = new mongoose.Schema({
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-  price: Number,
-  nama: String,
-  date: Date,
-  note: { type: String, default: "" },
-  status: { type: Boolean, default: false },
-  type: { type: String, enum: ["income", "expense"], required: true },
-});
+const TransactionSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    price: Number,
+    nama: String,
+    date: Date,
+    note: { type: String, default: "" },
+    status: { type: Boolean, default: false },
+    type: { type: String, enum: ["income", "expense"], required: true },
+  },
+  { timestamps: true }
+);
 
-const Transaction = mongoose.model("Transaction", TransactionSchema);
-export default Transaction;
+module.exports = mongoose.model("Transaction", TransactionSchema);
